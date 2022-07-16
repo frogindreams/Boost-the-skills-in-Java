ccceeed13344cbb0109e83a3d15fc8baa70d741d
frogindreams <frogharvard@gmail.com>
2022-07-16 19:51:38 +0900

    exception chapter: I

--- src/Test.java
+++ src/Test.java
@@ -1,27 +1,17 @@
-import java.io.FileNotFoundException;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Test
 {
     public static void main(String[] args) throws FileNotFoundException
     {
-        File file = new File("text.txt");
+        readAFile();
+    }
 
+    public static void readAFile() throws FileNotFoundException
+    {
+        File file = new File("text");
         Scanner scanner = new Scanner(file);
-        String line = scanner.nextLine();
-
-        String[] numbersString = line.split(" ");
-        int[] numbers = new int[3];
-        int counter = 0;
-
-        for (String number : numbersString)
-        {
-            numbers[counter++] = Integer.parseInt(number);
-        }
-
-        System.out.println(Arrays.toString(numbers));
-        scanner.close();
     }
 }
